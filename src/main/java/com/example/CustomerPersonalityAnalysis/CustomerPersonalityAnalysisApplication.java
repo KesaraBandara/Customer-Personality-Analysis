2005a0f1b27989d32f7eaad1092061f93ce5fe59
Harsha <kesrabandara1998@gmail.com>
2024-08-28 23:59:36 +0530

    first

---
+++ src/main/java/com/example/CustomerPersonalityAnalysis/CustomerPersonalityAnalysisApplication.java
@@ -0,0 +1,14 @@
+package com.example.CustomerPersonalityAnalysis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerPersonalityAnalysisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerPersonalityAnalysisApplication.class, args);
+		System.out.println("Project Started...");
+	}
+
+}
